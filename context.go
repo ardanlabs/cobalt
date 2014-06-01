ebae1782026fe65daefd24d93ce81155baa4c655
Ed Gonzo <Ed@ArdanStudios.com>
2014-05-31 22:13:50 -0400

    json helper methods for cache control.

--- context.go
+++ context.go
@@ -2,7 +2,12 @@ package cobalt
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+)
+
+const (
+	CacheControlHeader = "Cache-control"
 )
 
 type (
@@ -37,6 +42,22 @@ func (c *Context) SetData(key interface{}, value interface{}) {
 
 // ServeJson is a helper method to return json from a struct type.
 func (c *Context) ServeJson(obj interface{}) {
+	c.ServeJsonWithCache(obj, 0)
+}
+
+// ServeJsonWith is a helper method to return json from a struct type. It adds a cache control header
+// to the response if seconds > 0
+func (c *Context) ServeJsonWithCache(obj interface{}, seconds int64) {
+	if seconds > 0 {
+		c.Response.Header().Set(CacheControlHeader, fmt.Sprintf("private, must-revalidate, max-age=%d", seconds))
+	}
 	c.Response.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	json.NewEncoder(c.Response).Encode(obj)
+}
+
+// ServeJsonWithStatus is a helper method to return json from a struct type with a status code.
+func (c *Context) ServeJsonWithStatus(status int, obj interface{}) {
+	c.Response.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	c.Response.WriteHeader(status)
 	json.NewEncoder(c.Response).Encode(obj)
 }
