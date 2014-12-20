6c6cc69c0ccc2c7581e22bae10a329f04e5eea15
Ed Gonzo <Ed@ArdanStudios.com>
2014-12-19 22:51:52 -0500

    build script updated

--- zscripts/ci_build.sh
+++ zscripts/ci_build.sh
@@ -2,12 +2,8 @@
 echo
 echo "=> Build Starting..."
 echo
-echo "go get -u github.com/OutCast-IO/pat"
-
-go get -u github.com/OutCast-IO/pat || {exit 1;}
-
+echo bitbucket.org/ardanlabs/cobalt
+go clean -i bitbucket.org/ardanlabs/cobalt || { exit 1; }
+go build bitbucket.org/ardanlabs/cobalt || { exit 1; }
+go vet bitbucket.org/ardanlabs/cobalt || { exit 1; }
 echo
-echo "=> Compiling"
-echo
-
-go build -v || {exit;}
