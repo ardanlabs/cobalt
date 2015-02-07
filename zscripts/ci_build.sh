4b33ce3b7d0e4df4673826f31edec178b22bb353
Ed Gonzo <Ed@ArdanStudios.com>
2015-02-07 18:02:33 -0500

    build script change

--- zscripts/ci_build.sh
+++ zscripts/ci_build.sh
@@ -6,4 +6,6 @@ echo bitbucket.org/ardanlabs/cobalt
 go clean -i bitbucket.org/ardanlabs/cobalt || { exit 1; }
 go build bitbucket.org/ardanlabs/cobalt || { exit 1; }
 go vet bitbucket.org/ardanlabs/cobalt || { exit 1; }
+
+echo "=> Build Finished..."
 echo
