f12217dcb148ea7f3561865627d9ca9935a71289
Ed Gonzo <Ed@ArdanStudios.com>
2015-04-05 13:23:25 -0400

    ci_test script change

--- zscripts/ci_test.sh
+++ zscripts/ci_test.sh
@@ -1,5 +1,7 @@
 echo '=> Starting Tests...'
 
-go test -v -bench=. -benchtime=5s || { exit 1; }
+#go test -v -bench=. -benchtime=5s || { exit 1; }
 #go test -v -bench=. -benchtime=20s || { exit 1; }
 #go test -v -bench=. || { exit 1; }
+
+go test -v bitbucket.org/ardanlabs/cobalt
