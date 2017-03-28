42c2ee654e6c8cef28c3811792ed68343791b4be
Jacob Walker <jacobwalker0814@gmail.com>
2017-03-27 20:42:20 -0500

    Add Templates type for compiling and executing templates

---
+++ templates.go
@@ -0,0 +1,63 @@
+package cobalt
+
+import (
+	"html/template"
+	"io"
+	"path/filepath"
+)
+
+// Templates handles the compiling and execution of templates.
+type Templates struct {
+	Directory   string // The directory holding template files.
+	Extension   string // The file extension on templates.
+	Layout      string // The name of the base template that holds
+	Development bool   // Set to true to enable recompilation on each request
+
+	cache map[string]*template.Template
+}
+
+// DefaultTemplates creates a Templates set with default values.
+func DefaultTemplates() Templates {
+	return Templates{
+		Directory:   "templates",
+		Extension:   ".tmpl",
+		Layout:      "_layout",
+		Development: false,
+		cache:       make(map[string]*template.Template),
+	}
+}
+
+func (t Templates) lookup(name string) (*template.Template, error) {
+	if !t.Development {
+		if tmp, ok := t.cache[name]; ok {
+			return tmp, nil
+		}
+	}
+
+	l, err := template.New(t.Layout + t.Extension).ParseFiles(filepath.Join(t.Directory, t.Layout+t.Extension))
+	if err != nil {
+		return nil, err
+	}
+
+	n := filepath.Join(t.Directory, name+t.Extension)
+	tmp, err := l.ParseFiles(n)
+	if err != nil {
+		return nil, err
+	}
+
+	t.cache[name] = tmp
+
+	return tmp, nil
+}
+
+// Execute will load the named template and execute it against the provided
+// writer.
+func (t Templates) Execute(w io.Writer, name string, data interface{}) error {
+
+	tmp, err := t.lookup(name)
+	if err != nil {
+		return err
+	}
+
+	return tmp.Execute(w, data)
+}
---
+++ templates_test.go
@@ -0,0 +1,26 @@
+package cobalt
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/ardanlabs/cobalt"
+)
+
+func Test_TemplateHelloWorld(t *testing.T) {
+	tmp := cobalt.DefaultTemplates()
+	tmp.Directory = "_testdata/templates"
+
+	var buf bytes.Buffer
+
+	if err := tmp.Execute(&buf, "hello", "world"); err != nil {
+		t.Fatalf("Error should be nil, was %v", err)
+	}
+
+	want := "Body: Hello, world!"
+	if got := strings.TrimSpace(buf.String()); got != want {
+		t.Errorf("Got:  %s", got)
+		t.Errorf("Want: %s", want)
+	}
+}
